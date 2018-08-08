6781fc6a9d76dd4baf731dde9122c060b3e4d51d
George C. Privon <accounts5@privon.com>
2018-08-07 23:16:36 -0400

    save the output image

--- 01-arrow.rkt
+++ 01-arrow.rkt
@@ -2,6 +2,11 @@
 
 (require pict)
 
+(define (save-pict img fname ftype)
+  (define bm (pict->bitmap img))
+  (send bm save-file fname ftype))
+
 ; Draw a gray rectangle with a ligher gray arrow in the center
-(cc-superimpose (colorize (filled-rectangle 1920 1080) "dimgray")
+(save-pict (cc-superimpose (colorize (filled-rectangle 1920 1080) "dimgray")
                 (colorize (arrowhead 430 (/ pi 2)) "lightgray"))
+           "01-arrow.png" 'png)
