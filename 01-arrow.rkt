905fb616d20845faf399faeec5c46fb6453a0106
George C. Privon <accounts5@privon.com>
2018-08-08 11:53:56 -0400

    formatting tweak

--- 01-arrow.rkt
+++ 01-arrow.rkt
@@ -8,5 +8,5 @@
 
 ; Draw a gray rectangle with a ligher gray arrow in the center
 (save-pict (cc-superimpose (colorize (filled-rectangle 1920 1080) "dimgray")
-                (colorize (arrowhead 430 (/ pi 2)) "lightgray"))
+                           (colorize (arrowhead 430 (/ pi 2)) "lightgray"))
            "01-arrow.png" 'png)
