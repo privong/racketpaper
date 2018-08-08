b2735b5028ce617f0ccdd114ceea9290b48de157
George C. Privon <accounts5@privon.com>
2018-08-08 17:57:18 -0400

    some abstractions(?)

--- 01-arrow.rkt
+++ 01-arrow.rkt
@@ -6,7 +6,16 @@
   (define bm (pict->bitmap img))
   (send bm save-file fname ftype))
 
+(define (background color lx ly)
+  (colorize (filled-rectangle lx ly) color))
+
+(define (foreground color size angle)
+  (colorize (arrowhead size angle) color))
+
+(define (myscene bgcolor fgcolor lx ly)
+  (cc-superimpose (background bgcolor lx ly)
+                  (foreground fgcolor 430 (/ pi 2))))
+
 ; Draw a gray rectangle with a ligher gray arrow in the center
-(save-pict (cc-superimpose (colorize (filled-rectangle 1920 1080) "dimgray")
-                           (colorize (arrowhead 430 (/ pi 2)) "lightgray"))
+(save-pict (myscene "dimgray" "lightgray" 1920 1080)
            "01-arrow.png" 'png)
