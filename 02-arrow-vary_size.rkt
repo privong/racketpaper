7f429194feacab8a7393f8279b155532fb5b3d35
George C. Privon <accounts5@privon.com>
2018-09-03 23:09:21 -0300

    generate an arrow with a randomly-determined size

---
+++ 02-arrow-vary_size.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(require pict)
+
+(define (save-pict img fname ftype)
+  (define bm (pict->bitmap img))
+  (send bm save-file fname ftype))
+
+(define (background color lx ly)
+  (colorize (filled-rectangle lx ly) color))
+
+(define (foreground color size angle)
+  (colorize (arrowhead size angle) color))
+
+(define (myscene bgcolor fgcolor lx ly)
+  (cc-superimpose (background bgcolor lx ly)
+                  (foreground fgcolor (random (/ lx 20) (/ lx 4)) (/ pi 2))))
+
+; Draw a gray rectangle with a ligher gray arrow in the center
+(save-pict (myscene "dimgray" "lightgray" 1920 1080)
+           "02-arrow-random_size.png" 'png)
